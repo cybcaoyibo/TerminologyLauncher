9c894c82ae413ceb30df750845641cedab5f0cb0
CYBcaoyibo <cybcaoyibo@126.com>
2015-09-26 08:00:21 +0800

    add logging level

--- logger/logger.c
+++ logger/logger.c
@@ -20,20 +20,34 @@ void loggerCtor(logger *this) {
 	this->superObject.VT = &VT;
 	vectorCtor(&this->sinks);
 	if(uv_mutex_init(&this->mtx)) FAIL()
+	this->minLevel = 0xff;
 }
 
-void loggerAddSink(logger *this, loggerSink *sink) {
+void loggerAddSink(logger *this, loggerSink *sink, uint8_t minLevel) {
+	sink->minLevel = minLevel;
 	uv_mutex_lock(&this->mtx);
+	if(minLevel < this->minLevel) this->minLevel = sink->minLevel;
 	vectorPushBack(&this->sinks, sink);
 	uv_mutex_unlock(&this->mtx);
 }
 
-void loggerLog(logger *this, string *msg) {
+static bool loggerCheckLevel(logger *this, uint8_t level) {
+	uv_mutex_lock(&this->mtx);
+	bool rst = level >= this->minLevel;
+	uv_mutex_unlock(&this->mtx);
+	return rst;
+}
+
+static const char const *lvlStrs[] = {"VERBOSE", "DEBUG", "INFO", "WARN", "ERROR"};
+#define nLvlStrs 5
+
+void loggerLog(logger *this, uint8_t level, string *msg) {
+	if(!loggerCheckLevel(this, level)) return;
 	string str; stringCtor(&str);
 	time_t stamp = time(0);
 	struct tm info; memset(&info, 0, sizeof(info));
 	localtime_r(&stamp, &info);
-	stringAppendF(&str, "[%04d-%02d-%02d %02d:%02d:%02d][%" PRIu32 "]: ", info.tm_year, info.tm_mon, info.tm_mday, info.tm_hour, info.tm_min, info.tm_sec, GetCurrentThreadId());
+	stringAppendF(&str, "[%04d-%02d-%02d %02d:%02d:%02d][%" PRIu32 "][%s]: ", info.tm_year, info.tm_mon, info.tm_mday, info.tm_hour, info.tm_min, info.tm_sec, GetCurrentThreadId(), (level >= nLvlStrs) ? "UNKNOWN" : lvlStrs[level]);
 	stringAppend(&str, msg);
 	uv_mutex_lock(&this->mtx);
 	for(size_t i = 0; i < vectorSize(&this->sinks); i++) {
@@ -44,23 +58,23 @@ void loggerLog(logger *this, string *msg) {
 	dtorUtil(string, &str);
 }
 
-int loggerLogF(logger *this, const char *fmt, ...) {
+void loggerLogF(logger *this, uint8_t level, const char *fmt, ...) {
+	if(!loggerCheckLevel(this, level)) return;
 	va_list args;
 	va_start(args, fmt);
 	int sz = vsnprintf(0, 0, fmt, args);
 	if(sz < 0) {
 		va_end(args);
-		loggerLogF(this, "(vsnprintf failed)");
-		return sz;
+		loggerLogF(this, level, "(vsnprintf failed)");
+		return;
 	}
 	va_end(args);
 	va_start(args, fmt);
 	char *buff = malloc(sz + 1);
 	vsnprintf(buff, sz + 1, fmt, args);
 	string tmp; stringCtorFromCStrNTransfer(&tmp, buff, sz, true);
-	loggerLog(this, &tmp);
+	loggerLog(this,level, &tmp);
 	dtorUtil(string, &tmp);
 	va_end(args);
-	return sz;
 }
 
--- logger/logger.h
+++ logger/logger.h
@@ -4,6 +4,7 @@
 typedef struct logger {
 	vector sinks;
 	uv_mutex_t mtx;
+	uint8_t minLevel;
 	object superObject;
 } logger;
 
@@ -15,13 +16,20 @@ typedef struct loggerSinkVT {
 
 typedef struct loggerSink {
 	loggerSinkVT *VT;
+	uint8_t minLevel;
 	object superObject;
 } loggerSink;
 
 void loggerCtor(logger *this);
-void loggerAddSink(logger *this, loggerSink *sink);
-void loggerLog(logger *this, string *str);
-int loggerLogF(logger *this, const char *fmt, ...);
+void loggerAddSink(logger *this, loggerSink *sink, uint8_t minLevel);
+void loggerLog(logger *this, uint8_t level, string *str);
+void loggerLogF(logger *this, uint8_t level, const char *fmt, ...);
+
+#define LOG_VERBOSE 0
+#define LOG_DEBUG 1
+#define LOG_INFO 2
+#define LOG_WARN 3
+#define LOG_ERROR 4
 
 #endif
 
